05e6691e9c6dc0688b24c1823ba962b3c0f4e8f7
Reuben Roberts <r.roberts@nbn.org.uk>
2019-01-14 11:28:47 +0000

    Fix URLs when adding Alerts
    since we don't have the ALA 'records/' and 'records/ws' URLs but rather 'records/' frontend and 'records-ws/' backend

--- grails-app/assets/javascripts/species.show.js
+++ grails-app/assets/javascripts/species.show.js
@@ -134,9 +134,9 @@ function addAlerts(){
         var searchString = "?q=" + SHOW_CONF.guid;
         var url = SHOW_CONF.alertsUrl + "/webservice/createBiocacheNewRecordsAlert?";
         url += "queryDisplayName=" + encodeURIComponent(query);
-        url += "&baseUrlForWS=" + encodeURIComponent(SHOW_CONF.biocacheUrl);
+        url += "&baseUrlForWS=" + encodeURIComponent(SHOW_CONF.biocacheServiceUrl); /* was biocacheUrl */
         url += "&baseUrlForUI=" + encodeURIComponent(SHOW_CONF.serverName);
-        url += "&webserviceQuery=%2Fws%2Foccurrences%2Fsearch" + encodeURIComponent(searchString);
+        url += "&webserviceQuery=%2Foccurrences%2Fsearch" + encodeURIComponent(searchString); /* remove /ws/ */
         url += "&uiQuery=%2Foccurrences%2Fsearch%3Fq%3D*%3A*";
         url += "&resourceName=" + encodeURIComponent("Atlas");
         window.location.href = url;
