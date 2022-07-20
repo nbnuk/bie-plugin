b0c5c97cbdf8da522c10304401b4a03418afeb01
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-07-20 08:09:20 +0100

    extract sesarch.js phase II

--- grails-app/assets/javascripts/search.js
+++ grails-app/assets/javascripts/search.js
@@ -63,11 +63,6 @@ $(document).ready(function() {
     });
 });
 
-
-
-
-
-
 /**
  * Build URL params to remove selected fq
  *
@@ -109,18 +104,7 @@ function reloadWithParam(paramName, paramValue) {
     var q = $.getQueryParam('q') ? $.getQueryParam('q') : SEARCH_CONF.query ;
     var fqList = $.getQueryParam('fq'); //$.query.get('fq');
     var sort = $.getQueryParam('sortField');
-    if (sort == null || sort === undefined) {
-        sort = $('#sort-by').find(":selected").val();
-    }
     var dir = $.getQueryParam('dir');
-    if (dir == null || dir === undefined) {
-        dir = $('#sort-order').find(":selected").val();
-    }
-    var rows = $.getQueryParam('rows');
-    if (rows == null || rows === undefined) {
-        rows = $('#per-page').find(":selected").val();
-    }
-    var includeRecordsFilter = $.getQueryParam('includeRecordsFilter');
     // add query param
     if (q != null) {
         paramList.push("q=" + q);
@@ -130,19 +114,12 @@ function reloadWithParam(paramName, paramValue) {
         paramList.push("fq=" + fqList.join("&fq="));
     }
     // add sort param if already set
-    if (paramName != 'sortField' && (sort != null && sort !== undefined)) {
+    if (paramName != 'sortField' && sort != null) {
         paramList.push('sortField' + "=" + sort);
     }
     // add dir param if already set
     if (paramName != 'dir' && dir != null) {
         paramList.push('dir' + "=" + dir);
-    }
-    // add rows param if already set
-    if (paramName != 'rows' && rows != null) {
-        paramList.push('rows' + "=" + rows);
-    }
-    if (includeRecordsFilter) {
-        paramList.push('includeRecordsFilter' + '=' + includeRecordsFilter);
     }
     // add the changed value
     if (paramName != null && paramValue != null) {
@@ -150,6 +127,7 @@ function reloadWithParam(paramName, paramValue) {
     }
     //alert("paramName = " + paramName + " and paramValue = " + paramValue);
     //alert("params = "+paramList.join("&"));
+    //alert("url = "+window.location.pathname);
     window.location.href = window.location.pathname + '?' + paramList.join('&');
 }
 
@@ -193,11 +171,9 @@ function injectBhlResults() {
 }
 
 function injectBiocacheResults() {
+
     var queryToUse = (SEARCH_CONF.query == "" || SEARCH_CONF.query == "*" ? "*:*" : SEARCH_CONF.query);
-    if (queryToUse != "*:*") return; //new search cannot use this simple model for getting occurrence records
-    var biocacheContextUnencoded = $('<textarea />').html(SEARCH_CONF.biocacheQueryContext).text(); //to convert e.g. &quot; back to "
-    var url = SEARCH_CONF.biocacheServicesUrl + "/occurrences/search.json?q=" + queryToUse + "&start=0&pageSize=0&facet=off&qc=" + biocacheContextUnencoded;
-    console.log("url_biocache: " + url);
+    var url = SEARCH_CONF.biocacheServicesUrl + "/occurrences/search.json?q=" + queryToUse + "&start=0&pageSize=0&facet=off&qc=" + SEARCH_CONF.biocacheQueryContext;
     $.ajax({
         url: url,
         dataType: 'jsonp',
@@ -210,21 +186,6 @@ function injectBiocacheResults() {
     });
 }
 
-function injectBiocacheSearch(lsids, recsTot) {
-    var biocacheContextUnencoded = $('<textarea />').html(SEARCH_CONF.biocacheQueryContext).text(); //to convert e.g. &quot; back to "
-    var url = SEARCH_CONF.biocacheUrl + "/occurrences/search?q=lsid:(" + lsids + ")&qc=" + biocacheContextUnencoded;
-    var html = "<li data-count=\"" + recsTot + "\"><a href=\"" + url + "\" id=\"biocacheSearchLink\">Occurrence records</a> (" + numberWithCommas(recsTot) + ")</li>";
-    insertSearchLinks(html);
-}
-
-function injectBiocacheResultsActual(recsTot, limitSpp) {
-    var q = $.getQueryParam('q') ? $.getQueryParam('q') : SEARCH_CONF.query ;
-    var fqList = $.getQueryParam('fq');
-    var url = SEARCH_CONF.bieUrl + "/occurrences?q=" + q + (fqList? "&fq=" + fqList.join("&fq=") : "") + "&fq=" + SEARCH_CONF.recordsFilter;
-    var html = "<span class='biocacheRecordsLink'><a href=\"" + url + "\" id=\"biocacheRecordsLink\" title='View occurrences for up to " + limitSpp + " species'>View occurrence records</a> (" + numberWithCommas(recsTot) + ")</span>";
-    $(".record-cursor-details").append(html);
-}
-
 function insertSearchLinks(html) {
     // add content
     $("#related-searches ul").append(html);
@@ -234,6 +195,3 @@ function insertSearchLinks(html) {
     });
     $('#related-searches').removeClass('hide');
 }
-
-//= require leaflet.js
-
