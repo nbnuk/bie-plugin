b4f4e885fcce4309250d049b07d5f6558a189fb2
Keith Raven <k.raven@nbn.org.uk>
2022-07-28 17:25:02 +0100

    remove redundant code

--- grails-app/assets/javascripts/search.js
+++ grails-app/assets/javascripts/search.js
@@ -249,12 +249,6 @@ function injectBiocacheSearch(lsids, recsTot) {
 }
 
 function injectBiocacheResultsActual(recsTot, limitSpp) {
-    var q = $.getQueryParam('q') ? $.getQueryParam('q') : SEARCH_CONF.query ;
-    var fqList = $.getQueryParam('fq');
-    var url = SEARCH_CONF.bieUrl + "/occurrences?q=" + q + (fqList? "&fq=" + fqList.join("&fq=") : "") + "&fq=" + SEARCH_CONF.recordsFilter;
-    // var html = "<span class='biocacheRecordsLink'><a href=\"" + url + "\" id=\"biocacheRecordsLink\" title='View occurrences for up to " + limitSpp + " species'>View occurrence records</a> (" + numberWithCommas(recsTot) + ")</span>";
-    // $(".record-cursor-details").append(html);
-
     var html = "<button type='submit' class='btn btn-link brand-primary' style='padding-right: 0' title='View occurrences for up to " + limitSpp + " species'>View occurrence records</button> (" + numberWithCommas(recsTot) + ")";
 
     var allResultsGuids = MAP_CONF.allResultsGuids;
